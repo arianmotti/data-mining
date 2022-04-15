679ae5fd1eef0d58ec17d860c75d40cfc197defd
arianmotti <arianmottaghi2018@gmail.com>
2022-04-15 17:06:37 +0430

    plot function addded

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ for i in range(len(missing_data)):
         if missing_data.iloc[i][j]:
             Null_count[j] += 1
 print("missing data in each column:")
-print(Null_count)
+print('sepal_lenght : ', Null_count[0])
+print('sepal_width : ', Null_count[1])
+print('petal_length : ', Null_count[2])
+print('petal_width : ', Null_count[3])
+print('target : ', Null_count[4])
+
 df = df.dropna(axis=0, how='any')
 print(df)
 encoder = preprocessing.LabelEncoder()
@@ -26,8 +31,8 @@ variance = []
 mean = []
 
 for i in range(4):
-    variance.append(statistics.variance(df.iloc[i]))
-    mean.append(statistics.mean(df.iloc[i]))
+    variance.append(statistics.variance(df.iloc[:, i]))
+    mean.append(statistics.mean(df.iloc[:, i]))
 df2 = copy.deepcopy(df)
 del df2['target']
 scaler = preprocessing.StandardScaler()
@@ -37,9 +42,42 @@ standard_df = pd.DataFrame(standard_df, columns=[['sepal_length', 'sepal_width',
 normalized_variance = []
 normalized_mean = []
 for i in range(4):
-    normalized_variance.append(statistics.variance(standard_df.iloc[i]))
-    normalized_mean.append(statistics.mean(standard_df.iloc[i]))
+    normalized_variance.append(statistics.variance(standard_df.iloc[:, i]))
+    normalized_mean.append(statistics.mean(standard_df.iloc[:, i]))
 for i in range(4):
     print("column %2d" % (i + 1))
-    print("variance : before  %5.2f  after %5.2f" % (variance[i], normalized_variance[i]))
-    print("mean     : before  %5.2f  after %5.2f" % (mean[i], normalized_mean[i]))
+    print("variance : before  %6.2f  after %6.2f" % (variance[i], normalized_variance[i]))
+    print("mean     : before  %6.2f  after %6.2f" % (mean[i], normalized_mean[i]))
+
+principal = PCA(n_components=2)
+principal.fit(standard_df)
+data = principal.transform(standard_df)
+
+setosa = []
+versicolor = []
+virginica = []
+for i in range(len(data)):
+    if df.iloc[i]['target'] == 0:
+        setosa.append(data[i])
+    elif df.iloc[i]['target'] == 1:
+        versicolor.append(data[i])
+    else:
+        virginica.append(data[i])
+
+
+def plot(arr1, arr2, arr3):
+    plt.scatter([i[0] for i in setosa], [i[1] for i in arr1])
+    plt.scatter([i[0] for i in versicolor], [i[1] for i in arr2])
+    plt.scatter([i[0] for i in virginica], [i[1] for i in arr3])
+    plt.legend(["setosa", "versicolor", "virginica"])
+    plt.show()
+
+    visual_data = copy.deepcopy(df)
+    del visual_data['target']
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_axes([0, 0, 1, 1])
+    bp = ax.boxplot(visual_data)
+    plt.show()
+
+
+plot(setosa, versicolor, virginica)
